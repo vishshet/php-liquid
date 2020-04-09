7a6defb3e8e0bfbf444cc1d13b34a143c261b946
Vishwesh Shetty <vishwesh.shetty@adevole.com>
2020-04-09 17:17:35 +0530

    add 'extended for CartX'

--- src/Liquid/Template.php
+++ src/Liquid/Template.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Liquid package.
+ * This file is part of the Liquid package extended for CartX
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
